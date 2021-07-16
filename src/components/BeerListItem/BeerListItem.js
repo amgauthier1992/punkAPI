25992d1d34bd99289858aa859b22b72f17576fe6
Andy Gauthier <amgauthier1992@gmail.com>
2021-07-16 10:43:51 -0400

    altered style rules for beer card/item content and grid layout

--- src/components/BeerListItem/BeerListItem.js
+++ src/components/BeerListItem/BeerListItem.js
@@ -22,11 +22,8 @@ export default function BeerListItem(props){
     <div className="beer-list-item">
       <img className="beer-img" src={props.beerImg} alt={props.beerName}/>
       <h2>{props.beerName}</h2>
+      <div className="beer-metrics"><p>{abvJSX} / {ibuJSX}</p></div>
       <p className="item-p">Description: {descriptionJSX}</p>
-      <div className="beer-metrics">
-        <p className="item-p">{abvJSX}</p>
-        <p className="item-p">{ibuJSX}</p>
-      </div>
     </div>
   );
 };
